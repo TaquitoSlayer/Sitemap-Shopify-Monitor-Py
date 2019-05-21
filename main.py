1e46b16fb6c89be0de905620ef6118b92373b1d3
Taquito Slayer <yaboy@yaboy.com>
2019-05-21 13:23:41 -0600

    embed update and better kith handling

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import requests
 import logging
 import products
 import proxyhandler
-from multiprocessing import Process, Lock
+from threading import Thread
 import time
 import json
-from discord_hooks import Webhook
+from dhooks import Webhook, Embed
 from urllib.parse import urlparse
 import re
 
@@ -19,13 +19,12 @@ sites = products.generate_sitelist()
 def config():
     with open('config.json') as json_file:
         config = json.load(json_file)
-        tasks = config['tasks-per-site']
+        tasks = 1
         delay = config['delay']
-        webhook = config['webhook']
-    return tasks, delay, webhook
+    return tasks, delay
 
 # executing now to prevent confusion
-tasks, delay, webhooks = config()
+tasks, delay = config()
 print(f'SHOPIFY MONITOR BY @TAQUITOSLAYER - {tasks} TASKS PER SITE WITH A DELAY OF {delay} SECONDS PER PROXY BAN')
 
 def check_if_posted(url):
@@ -39,23 +38,45 @@ def check_if_posted(url):
     
 
 def post_to_discord(product_url):
-    title, image, _stock, price, product_url, variants = products.get_info(product_url)
+    fucked = False
+    while not fucked:
+        try:
+            proxy_picked = proxyhandler.proxy()
+            title, image, _stock, price, product_url, variants = products.get_info(product_url, proxy_picked)
+            fucked = True
+        except:
+            pass
     parsed_uri = urlparse(product_url)
     result = '{uri.netloc}'.format(uri=parsed_uri)
     eve_qt = 'http://remote.eve-backend.net/api/quick_task?link=' + product_url
-    for webhook in webhooks:
-        embed = Webhook(webhook, color=1118481)
-        embed.set_desc(f'[{title}]({product_url})')
-        embed.add_field(name='Price',value='{}'.format(price))
-        links = []
-        for vid, titlez in variants.items():
-            links.append(f'[{titlez}](http://{result}/cart/{vid}:1)\n')
-        links = ''.join(links)
-        embed.add_field(name='Sizes Available', value=links,inline='false')
-        embed.add_field(name='Quick Tasks', value=f'[EVE]({eve_qt})',inline='false')
-        embed.set_thumbnail(image)
-        embed.set_footer(text=f'Shopify Monitor by @TaquitoSlayer | {result}', ts=True)
-        embed.post()
+    cyber_qt = 'https://cybersole.io/dashboard/quicktask?url=' + product_url
+    pd_qt = 'https://api.destroyerbots.io/quicktask?url=' + product_url
+    tks_qt = 'https://thekickstationapi.com/quick-task.php?link=' + product_url
+    sb_qt = 'https://scottbotv1.com/quicktask?' + product_url
+    swft_qt = 'http://swftaio.com/pages/quicktask?Input=' + product_url
+    with open('webhook.json') as json_file:
+        json_dump = json.load(json_file)
+        for site_name in json_dump:
+            if site_name in result:
+                # webhookz = json_dump[site_name]['webhook']
+                webhookz = ['https://discordapp.com/api/webhooks/555789296431267875/WqiqWrb6KJQ3ZwS9-jJyxgglrIwxRGfPO2_i_L5xxmUy0UZx8bDNlfGwXYF-j19YASAs']
+                embed = Embed()
+                for webhook in webhookz:
+                    client = Webhook(webhook)
+                    embed.color = 0xFF0000
+                    embed.description = f'[{title}]({product_url})'
+                    price = int(price/100)
+                    embed.add_field(name='Price',value=f'${price}')
+                    links = []
+                    for vid, titlez in variants.items():
+                        links.append(f'[{titlez}](http://{result}/cart/{vid}:1)\n')
+                    links = ''.join(links)
+                    embed.add_field(name='Sizes Available', value=links,inline='false')
+                    embed.add_field(name='Quick Tasks', value=f'[EVE]({eve_qt}) - [CYBER]({cyber_qt}) - [PD]({pd_qt}) - [TKS]({tks_qt}) - [SB]({sb_qt}) - [SWFT]({swft_qt})',inline='false')
+                    embed.set_thumbnail(image)
+                    embed.set_footer(text=f'Shopify Monitor (XML) by @TaquitoSlayer | {result}')
+                    client.send(embeds=[embed])
+                    embed.fields.clear()
 
 def channel_fill():
     fucked = False
@@ -73,55 +94,54 @@ def channel_fill():
             print(e)
             time.sleep(float(delay))
             pass
+    
 
 def monitor(url, proxy, task_num):
     try:
         initial_product_list = products.List(url, proxy)
-        print(initial_product_list)
-        # initial_product_list = products.List2()
+        initial_product_list = []
     except requests.exceptions.RequestException as err:
         logging.info(f'{url.upper()} - {task_num} - {task_num}: ERROR: ' + err)
-    try:
-        while True:
-            try:
-                new_product_list = products.List(url, proxy)
-                # new_product_list = products.List3()
-            except requests.exceptions.RequestException as err:
-                logging.info(f'{url.upper()} - {task_num}: ERROR: ' + err)
+        pass
+    while True:
+        try:
+            new_product_list = products.List(url, proxy)
+            new_product_list = ['https://kith.com/products/nike-shox-tl-triple-black']
+        except requests.exceptions.RequestException as err:
+            logging.info(f'{url.upper()} - {task_num} - {task_num}: ERROR: ' + err)
+            pass
 
-            diff = list(set(new_product_list) - set(initial_product_list))
-            if bool(diff) == True:
-                logging.info(f'{url.upper()} - {task_num}: NEW PRODUCT FOUND!')
-                diff = set(diff)
-                for product in diff:
-                    check_if_posted(product)
-                    initial_product_list = new_product_list
-                    time.sleep(2)
+        diff = list(set(new_product_list) - set(initial_product_list))
 
-            elif bool(diff) == False:
-                logging.info(f'{url.upper()} - {task_num}: NO CHANGES FOUND')
-                pass
-            else:
-                pass
-    except Exception as e:
-        print(e)
+        if bool(diff) == True:
+            logging.info(f'{url.upper()} - {task_num}: NEW PRODUCT FOUND!')
+            diff = set(diff)
+            for product in diff:
+                check_if_posted(product)
+                initial_product_list = new_product_list
+                time.sleep(2)
+        elif bool(diff) == False:
+            logging.info(f'{url.upper()} - {task_num}: NO CHANGES FOUND')
+            pass
+        else:
+            pass
 
 def main(task_num, url, delay):
     fucked = False
     while not fucked:
         proxy_picked = proxyhandler.proxy()
-        try:
-            monitor(url, proxy_picked, task_num)
-            fucked = True
+        # try:
+        monitor(url, proxy_picked, task_num)
+        fucked = True
         # simplejson.errors.JSONDecodeError
-        except Exception as e:
-            logging.info(f'{url.upper()} SOMETHING WRONG, PROBABLY PROXY BAN - {task_num}: {proxy_picked} - SLEEPING FOR {delay} SECONDS')
-            logging.info(f'{e}')
-            time.sleep(float(delay))
-            pass
+        # except Exception as e:
+        #     logging.info(f'{url.upper()} SOMETHING WRONG, PROBABLY PROXY BAN - {task_num}: {proxy_picked} - SLEEPING FOR {delay} SECONDS')
+        #     logging.info(f'{e}')
+        #     time.sleep(float(delay))
+        #     pass
 
 if __name__ == '__main__':
     for site in sites:
         for i in range(int(tasks)):
-            p = Process(target=main, args=(i+1, site, delay))
+            p = Thread(target=main, args=(i+1, site, delay))
             p.start() # starting workers
--- products.py
+++ products.py
@@ -11,6 +11,7 @@ def generate_sitelist():
         sitelist = urls.read().splitlines()
     return sitelist
 
+
 def List(url, proxy):
     product_urls = []
     sitemap_products = []
@@ -18,12 +19,24 @@ def List(url, proxy):
     soup = bs.BeautifulSoup(dump.text, 'lxml')
     for sitemap in soup.find_all('loc'):
         sitemap_products.append(sitemap.get_text())
-    sitemap_products = sitemap_products[0]
-    dump_products = r.get(sitemap_products, headers = headers, proxies={"http": proxy, "https": proxy})
-    soup = bs.BeautifulSoup(dump_products.text, 'lxml')
-    for product in soup.find_all('loc'):
-        product_urls.append(product.get_text())
-    
+    if 'kith' in url:
+        sitemap_productz = sitemap_products[1]
+        dump_products = r.get(sitemap_productz, headers = headers, proxies={"http": proxy, "https": proxy})
+        soup = bs.BeautifulSoup(dump_products.text, 'lxml')
+        for product in soup.find_all('loc'):
+            product_urls.append(product.get_text())
+        sitemap_productz = sitemap_products[0]
+        dump_products = r.get(sitemap_productz, headers = headers, proxies={"http": proxy, "https": proxy})
+        soup = bs.BeautifulSoup(dump_products.text, 'lxml')
+        for product in soup.find_all('loc'):
+            product_urls.append(product.get_text())  
+    else:
+        sitemap_products = sitemap_products[0]
+        dump_products = r.get(sitemap_products, headers = headers, proxies={"http": proxy, "https": proxy})
+        soup = bs.BeautifulSoup(dump_products.text, 'lxml')
+        for product in soup.find_all('loc'):
+            product_urls.append(product.get_text())
+    product_urls = list(set(product_urls))
     return product_urls
 
 def List2():
@@ -49,67 +62,53 @@ def List3():
     return product_urls
 
 
-def get_info(url):
+def get_info(url, proxy):
     variants = {}
-    resp = r.get(url, headers = headers)
-    stock = 'N/A'
-    image = 'https://i.imgur.com/PheG08Z.jpg'
-    title = 'NO NAME FOUND'
-    price = 'N/A'
-    
+    resp = r.get(url, headers = headers, proxies={"http": proxy, "https": proxy})
+    # resp = r.get(url, headers = headers)
     try:
         stock = re.findall(r'''"inventory_quantity":(\d*),''', resp.text)
         stock = stock[0]
     except:
+        stock = 'N/A'
         pass
     try:
-        jsonurl = url + '.json'
-        resp_json = r.get(jsonurl, headers = headers)
+        jsonurl = url + '.js'
+        resp_json = r.get(jsonurl, headers = headers, proxies={"http": proxy, "https": proxy})
         resp_json = json.loads(resp_json.text)
     except:
         print('ERROR LOADING PRODUCT JSON')
         pass
 
     try:
-        images = resp_json['product']['images']
-        images = images[0]
-        image = images['src']
+        image = resp_json['images'][0]
+        image = f'https:{image}'
     except:
+        image = 'https://i.imgur.com/PheG08Z.jpg'
+        pass
+    
+    try:
+        title = resp_json['title']
+    except:
+        title = 'NO NAME FOUND'
         pass
     try:
-        title = resp_json['product']['title']
-    except:
-        pass
-    try:
-        prices = resp_json['product']['variants']
+        prices = resp_json['variants']
         for price in prices:
             price = price['price']
     except Exception as e:
         print(e)
+        price = 'N/A'
         pass
     try:
-        variantz = resp_json['product']['variants']
-        if 'kith' in url:
-            tags = resp_json['product']['tags']
-            tags = re.findall(r'''productsize-(.*)?, productsize-''', tags)
-            tags = tags[0].replace('productsize-', '')
-            tags = tags.replace('-', '.')
-            tags = tags.replace(' ', '')
-            tags = tags.split(',')
-            for variant in variantz:
-                for tag in tags:
-                    if tag == variant['title']:
-                        vid = variant['id']
-                        name = variant['title']
-                        variants[vid] = name
-                    else:
-                        pass
-        else:
-            for variant in variantz:
+        variantz = resp_json['variants']             
+        for variant in variantz:
+            if variant['available'] == True:
                 vid = variant['id']
                 name = variant['title']
                 variants[vid] = name
+            else:
+                pass
     except Exception as e:
-        print(e)
-        variants = {"NO ATC LINK FOUND" : "YOU-PLAYED-YOURSELF"}
+        print(f'ERROR: {e}')
     return title, image, stock, price, url, variants
